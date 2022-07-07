f28499715b0e0e47a4b08792547241090a0ae0b6
cdh <cdh@33.cn>
2022-07-07 17:06:18 +0800

    chore(ycc):update ycc version, READEME.md

--- chain33/ycc/main_eth_address/READEME.md
+++ chain33/ycc/main_eth_address/READEME.md
@@ -1,7 +1,7 @@
 YCC公网配置和脚本
 
 代码仓库
-https://github.com/yccproject/ycc/commits/master 80377d035e566a6d2bd93ca70ca3d40c44fe2c02
+https://github.com/yccproject/ycc/commits/master 1c3539519ef6d5daa44ef740004dc06541fa02b7
 
 节点运行指令
 make run
@@ -16,9 +16,27 @@ bash wallet-genesis.sh
 bash wallet-init.sh
 
 搭建步骤
-1.拉取代码仓库,将本目录下的ycc.go替换代码仓库的ycc.go文件,make build编译代码
+1.拉取代码仓库 make build编译代码
 2.将编译好的ycc和ycc-cli和本目录下的脚本都拷贝到服务器上
 3.搭建初始化节点,make run运行，bash wallet-genesis.sh初始化挖矿
 4.搭建共识节点，修改 配置文件下p2p.sub.dht的seeds和consensus.sub.pos33下的bootPeers,
 将ip和端口改为初始节点的ip和对应的端口,make run运行,bash wallet-init.sh初始化共识节点,
 等高度同步后再执行make bind进行绑定挖矿
+
+类ycc链的搭建：
+需要根据需求修改ycc.go和ycc.toml文件
+1.初始节点:初始节点里一定要修改的是管理员地址,ycc.go里exec.sub.manage下的superManager,
+建议使用钱包生成eth的私钥地址,填入地址即可
+挖矿地址(consensus.sub.pos33.genesis下的minerAddr)和委托挖矿地址(returnAddr)可以修改(不是必须),
+修改后需要将脚本里的私钥也换成对应的私钥
+ycc.toml里需要将seeds和bootPeers的地址都注释掉
+singleMode改为true
+修改ChainID,需要未注册过的,可以在https://chainlist.org/zh上确认是否注册过
+类ycc可以支持小狐狸钱包,如果要使用的话,需要先去https://github.com/ethereum-lists 提交新链信息的pr
+参考ycc的pr https://github.com/ethereum-lists/chains/pull/1223/files
+
+2.共识节点:编译出来的程序要和初始节点一致
+ycc.toml里singleMode为false
+seeds和bootPeers的地址需要填入初始节点和其他节点的p2p信息
+本节点的p2p信息可以通过./ycc-cli --rpc_laddr=http://localhost:9901 net peer 指令查看
+修改ChainID,和初始节点保持一致
--- chain33/ycc/main_eth_address/wallet-init.sh
+++ chain33/ycc/main_eth_address/wallet-init.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 function init() {
-    main_jrpc="http://192.168.11.123:7905"
+    main_jrpc="http://localhost:9901"
     echo "=========== # start set wallet 1 ============="
     echo "=========== # save seed to wallet ============="
-    result=$(./ycc-cli --rpc_laddr=http://localhost:7905 seed generate -l 0)
-    result=$(./ycc-cli --rpc_laddr=http://localhost:7905 seed save -p 1314fuzamei -s "${result}" | jq ".isok")
+    result=$(./ycc-cli --rpc_laddr=${main_jrpc} seed generate -l 0)
+    result=$(./ycc-cli --rpc_laddr=${main_jrpc} seed save -p 1314fuzamei -s "${result}" | jq ".isok")
     if [ "${result}" = "false" ]; then
         echo "save seed to wallet error seed, result: ${result}"
         exit 1
@@ -14,7 +14,7 @@ function init() {
     sleep 1
 
     echo "=========== # unlock wallet ============="
-    result=$(./ycc-cli --rpc_laddr=http://localhost:7905 wallet unlock -p 1314fuzamei -t 0 | jq ".isok")
+    result=$(./ycc-cli --rpc_laddr=${main_jrpc} wallet unlock -p 1314fuzamei -t 0 | jq ".isok")
     if [ "${result}" = "false" ]; then
         exit 1
     fi
@@ -22,74 +22,22 @@ function init() {
     sleep 1
 
     echo "=========== # create new key for transfer ============="
-    transfer_addr=$(./ycc-cli --rpc_laddr=http://localhost:7905 account create -l transfer | jq ".acc.addr"| sed -r 's/"//g')
-    echo "${transfer_addr}"
-    if [ -z "${transfer_addr}" ]; then
+    result=$(./ycc-cli --rpc_laddr=${main_jrpc} account create -t 2 -l transfer | jq ".acc")
+    echo "${result}"
+    if [ -z "${result}" ]; then
         exit 1
     fi
 
-    echo "=========== # get transfer key ============="
-    transfer_prikey=$(./ycc-cli --rpc_laddr=http://localhost:7905 account dump_key -a ${transfer_addr} | jq ".data" | sed -r 's/"//g')
-    echo "${transfer_prikey}"
-    if [ -z "${transfer_prikey}" ]; then
-        exit 1
-    fi
 
+    sleep 1
     echo "=========== # create new key for mining ============="
-    mining_addr=$(./ycc-cli --rpc_laddr=http://localhost:7905 account create -l mining | jq ".acc.addr" | sed -r 's/"//g')
-    echo "${mining_addr}"
-    if [ -z "${mining_addr}" ]; then
-        exit 1
-    fi
-
-    echo "=========== # get mining key ============="
-    mining_prikey=$(./ycc-cli --rpc_laddr=http://localhost:7905 account dump_key -a ${mining_addr} | jq ".data" | sed -r 's/"//g')
-    echo "${mining_prikey}"
-    if [ -z "${mining_prikey}" ]; then
-        exit 1
-    fi
-
-    sleep 1
-    echo "=========== # send to transfer_addr  ============="
-    result=$(./ycc-cli --rpc_laddr=${main_jrpc} send coins transfer -a=100003000 -t ${transfer_addr} -k CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944)
+    result=$(./ycc-cli --rpc_laddr=${main_jrpc} account create -t 2 -l mining | jq ".acc")
     echo "${result}"
     if [ -z "${result}" ]; then
         exit 1
     fi
 
-    echo "=========== # send to mining_addr  ============="
-    result=$(./ycc-cli --rpc_laddr=${main_jrpc} send coins transfer -a=100003000 -t ${mining_addr} -k CC38546E9E659D15E6B4893F0AB32A06D103931A8230B0BDE71459D2B27D6944)
-    echo "${result}"
-    if [ -z "${result}" ]; then
-        exit 1
-    fi
-
-    sleep 1
-    echo "=========== # transfer_addr deposit to pos33  ============="
-    deposit_hash=$( ./ycc-cli --rpc_laddr=${main_jrpc} send coins transfer -a=100000000 -t 1Wj2mPoBwJMVwAQLKPNDseGpDNibDt9Vq -k ${transfer_prikey})
-    echo "${deposit_hash}"
-    if [ -z "${deposit_hash}" ]; then
-        exit 1
-    fi
-   
-    sleep 1
-    echo "=========== # transf_account entrust mining  ============="
-    entrust_hash=$(./ycc-cli --rpc_laddr=${main_jrpc} send pos33 entrust -a 1000000 -e ${mining_addr}  -r ${transfer_addr} -k ${transfer_prikey})
-    echo "${entrust_hash}"
-    if [ -z "${entrust_hash}" ]; then
-        exit 1
-    fi
-
-    sleep 1
-    echo "=========== # check deposit and entrust result  ============="
-    transfer_account=$(./ycc-cli --rpc_laddr=${main_jrpc} account balance -a ${transfer_addr}  | jq ".execAccount[0]")
-    echo "${transfer_account}"
-    if [[${transfer_account}==""]||[${transfer_account}|jq ".execer"!="pos33"]||[${transfer_account}|jq ".account.frozen"<1000000]]; then
-        exit 1
-    fi
-
-
-    echo "=========== # end  ============="
+    echo "=========== # end set wallet 1 ============="
 
 }
 
